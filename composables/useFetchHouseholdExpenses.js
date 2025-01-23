6209ba673e3d6aa79b91e7c89266471f8d06532f
HesamSameni <hesam.sameni@gmail.com>
2025-01-23 16:03:43 +0000

    fixing expenses page (#21)

--- composables/useFetchHouseholdExpenses.js
+++ composables/useFetchHouseholdExpenses.js
@@ -1,13 +1,37 @@
-export const useFetchHouseholdExpenses = (
-  period,
-  householdId,
-  householdUsers
-) => {
+export const useFetchHouseholdExpenses = (period, householdId) => {
   const supabase = useSupabaseClient();
   const pending = ref(false);
   const expenses = ref([]); // Holds the entire list of expenses
+  const boardMembers = ref([]);
 
   watch(period, async () => await refresh(householdId));
+
+  const getCurrentBoardMembers = async (householdId) => {
+    pending.value = true;
+
+    try {
+      // Step 1: Fetch the board IDs where the current user is the owner
+      const { data: data, error: boardMembersError } = await supabase
+        .from("board_members")
+        .select("user_id")
+        .eq("board_id", householdId);
+      if (boardMembersError) {
+        console.error("Error fetching board members:", boardMembersError);
+        return [];
+      }
+
+      const boardMembers = data.filter(
+        (o, index, arr) =>
+          arr.findIndex(
+            (item) => JSON.stringify(item) === JSON.stringify(o)
+          ) === index
+      );
+
+      return boardMembers;
+    } finally {
+      pending.value = false;
+    }
+  };
 
   const fetchExpenses = async (householdId) => {
     pending.value = true;
@@ -17,9 +41,9 @@ export const useFetchHouseholdExpenses = (
         `expenses-${period.value.from.toDateString()}-${period.value.to.toDateString()}`,
         async () => {
           const { data, error } = await supabase
-            .from("temp_household_transactions")
+            .from("board_expenses")
             .select()
-            .eq("household_id", householdId)
+            .eq("board_id", householdId)
             .gte("purchase_date", period.value.from.toISOString())
             .lte("purchase_date", period.value.to.toISOString())
             .order("purchase_date", { ascending: false });
@@ -38,18 +62,22 @@ export const useFetchHouseholdExpenses = (
     expenses.value = await fetchExpenses(householdId);
   };
 
+  const getUsers = async (householdId) => {
+    boardMembers.value = await getCurrentBoardMembers(householdId);
+  };
+
   const expensesByUser = computed(() => {
     const userExpenses = {};
-    householdUsers.forEach((user) => {
-      userExpenses[user.name] = expenses.value.filter(
-        (expense) => expense.paid_by === user.name
+    boardMembers.value.forEach((user) => {
+      userExpenses[user.user_id] = expenses.value.filter(
+        (expense) => expense.paid_by === user.user_id
       );
     });
     return userExpenses;
   });
 
-  const getUserExpenses = (userName) => {
-    const userExpenses = expensesByUser.value[userName] || [];
+  const getUserExpenses = (userId) => {
+    const userExpenses = expensesByUser.value[userId] || [];
     return userExpenses.reduce(
       (total, expense) => total + (expense.amount || 0),
       0
@@ -61,7 +89,11 @@ export const useFetchHouseholdExpenses = (
       all: expenses, // Include the entire list of expenses
       getUserExpenses,
     },
+    boardMembers: {
+      all: boardMembers,
+    },
     refresh,
     pending,
+    getUsers,
   };
 };
