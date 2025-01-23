b2c6200ca48f834b59d8e9b6e7cfdcd48946e129
Hesam Sameni <hesam.sameni@gmail.com>
2025-01-23 13:14:06 +0000

    fixing modal issue

--- components/add-expense-modal.vue
+++ components/add-expense-modal.vue
@@ -1,5 +1,10 @@
 <template>
-  <UModal v-model="isOpen">
+  <UModal
+    v-model="isOpen"
+    :ui="{
+      container: 'min-h-0 lg:min-h-full',
+    }"
+  >
     <UCard>
       <template #header> Add Expense </template>
       <UForm :state="state" :schema="schema" ref="form">
